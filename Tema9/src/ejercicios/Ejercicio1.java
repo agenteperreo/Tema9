5b49fa89c5e412fd6a3e80c052d384ed02f474d8
isaac <igarcar@iesnervion.es>
2023-05-22 21:19:01 +0200

    22/05/2023

--- Tema9/src/ejercicios/Ejercicio1.java
+++ Tema9/src/ejercicios/Ejercicio1.java
@@ -6,8 +6,10 @@ public class Ejercicio1 {
 
 	public static void main(String[] args) {
 
+		// Creamos el linkedhashset
 		LinkedHashSet<Integer> numeros = new LinkedHashSet<Integer>();
 
+		// Generamos 20 numeros aleatorios y los añadimos al linkedhashset
 		for (int i = 0; i < 20; i++) {
 
 			int num = (int) (Math.random() * 100 + 1);
@@ -15,6 +17,7 @@ public class Ejercicio1 {
 			numeros.add(num);
 		}
 
+		// Mostramos los numero creados
 		System.out.println(numeros);
 	}
 }
